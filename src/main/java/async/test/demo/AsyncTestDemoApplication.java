2f8bf0f22801c448d07217ba912571c5c0a0a6e4
s4055 <sijin901@gmail.com>
2025-04-21 00:35:05 +0900

    feat: project init
    feat: project init

---
+++ src/main/java/async/test/demo/AsyncTestDemoApplication.java
@@ -0,0 +1,13 @@
+package async.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncTestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/async/test/demo/AsyncTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package async.test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
